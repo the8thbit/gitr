7455c26361e2760c2d8d1b34a018659b14466021
Peter Sussman <psussman@emacinc.com>
2014-11-10 14:41:43 -0600

    added structure to allow for extending git functionality

--- server.js
+++ server.js
@@ -7,24 +7,38 @@ if (!which('git')) {
 }
 
 //get parameters
-var params = [];
+var node_params = [];
 process.argv.forEach(function (val, index, array) {
 	if (index >= 2) {
 		if (val.indexOf(' ') >= 0) {
-			params.push("\"" + val + "\"");
+			node_params.push("\"" + val + "\"");
 		} else {
-			params.push(val);
+			node_params.push(val);
 		}
 	}
 });
 
-run_git(params);
+var git_params = gen_git_params(node_params);
+run_com("git", git_params);
 
-function run_git(params) {
-	var exec_string = "git " + params.join(" ");
+
+function gen_git_params(params) {
+	git_params = [];
+	if (false) {
+		//do something
+	} else if (false) {
+		//do something else
+	} else {
+		git_params = params;
+	}
+	return git_params;
+}
+
+function run_com(command, params) {
+	var exec_string = command + " " +  params.join(" ");
 	// Run external tool synchronously
 	if (exec(exec_string).code !== 0) {
-		echo('Error: git failed.');
+		echo('Error: execution failed.');
 		exit(1);
 	}
 }
