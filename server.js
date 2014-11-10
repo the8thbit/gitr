3bee147f97ea1bc797bb63d873dfb803f8debd3f
Peter Sussman <psussman@emacinc.com>
2014-11-10 13:55:58 -0600

    made program act like an alias of git

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ if (!which('git')) {
 	exit(1);
 }
 
-// Run external tool synchronously
-if (exec('git status').code !== 0) {
-  echo('Error: git failed.');
-  exit(1);
-}
+var exec_string = "git";
+process.argv.forEach(function (val, index, array) {
+	if(index >= 2) {
+		exec_string += " " + val;
+	}
+});
 
+console.log(exec_string);
+
+// Run external tool synchronously
+if (exec(exec_string).code !== 0) {
+	echo('Error: git failed.');
+	exit(1);
+}
