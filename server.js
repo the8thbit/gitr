70b998aa91339f5447b0e9619be7828374583963
Peter Sussman <psussman@emacinc.com>
2014-11-10 13:44:34 -0600

    made git the focus rather than sl CHOOO CHOOOOOOOO

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('shelljs/global');
 
 if (!which('git')) {
-	echo('Sorry, this script requires sl');
+	echo('Sorry, this script requires git.');
 	exit(1);
 }
 
 // Run external tool synchronously
-if (exec('sl').code !== 0) {
-  echo('Error: sl failed.');
+if (exec('git status').code !== 0) {
+  echo('Error: git failed.');
   exit(1);
 }
 
